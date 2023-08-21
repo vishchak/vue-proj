ae5d3b05ed4938295bd6cef5850a55919447250c
Denis <denis.vishchak@gmail.com>
2023-08-21 14:13:46 +1000

    render sample message in browser;

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <MySkills/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MySkills from './components/skills.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MySkills
   }
 }
 </script>
